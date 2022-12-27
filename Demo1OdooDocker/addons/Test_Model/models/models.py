d95c735703c951d50f2f2ebccfa2ac256a251b50
AndresCRR <andreskmilorr97@gmail.com>
2022-12-27 15:20:31 -0500

    quinto commit

--- Demo1OdooDocker/addons/Test_Model/models/models.py
+++ Demo1OdooDocker/addons/Test_Model/models/models.py
@@ -1,10 +1,14 @@
+
+from dateutil.relativedelta import relativedelta
 from odoo import models, fields
 
 class TestModel(models.Model):
     _name = "test.model"
     _description = "Test Model"
 
-    name = fields.Char()
+    name = fields.Char(default="Unknown")
+    last_seen = fields.Datetime("Last Seen", default=lambda self: fields.Datetime.now())
+
 
 class estate_property(models.Model):
     _name = 'estate_property.model'
@@ -13,15 +17,17 @@ class estate_property(models.Model):
     name = fields.Char(string='Title',required=True)
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
-    date_availability = fields.Date(string='Available From')
+    date_availability = fields.Date(string='Available From',default=lambda self: fields.Date.today()+ relativedelta(months=3))
     expected_price = fields.Float(string='Expected price',required=True)
     selling_price = fields.Float(string='Selling Price')
-    bedrooms = fields.Integer(string='Bedrooms')
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
-    garden_orientation = fields.Selection([('N','North'),('S','South'),('E','East'),('W','West')])
+    bedrooms = fields.Integer(string='Bedrooms', default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer(string='Facades')
+    garage = fields.Boolean(string='Garage')
+    garden = fields.Boolean(string='Garden')
+    garden_area = fields.Integer(string='Garden Area (sqm)')
+    garden_orientation = fields.Selection([('N','North'),('S','South'),('E','East'),('W','West')], string='Garden Orientation')
+    state = fields.Selection([('n','New'),('OfA','Offert Acepted'),('OfR','Offert Recived'),('S','Sold'),('C','Canceled')], string='Status')
+    active = fields.Boolean(string='Active')
 
 
